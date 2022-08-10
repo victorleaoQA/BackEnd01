3e1066270fab8636e7da6666689bdd66f5fd616c
victorleao <leaovic@gmail.com>
2022-08-10 17:23:29 -0300

    Adicionado ChainOfResponsability_Exemplo

--- src/Aula04_ChainOfResponsabilityPattern/Exemplo/Program.java
+++ src/Aula04_ChainOfResponsabilityPattern/Exemplo/Program.java
@@ -5,9 +5,9 @@ public class Program {
         FuncionarioBanco handlerBase = new
                 ExecutivoDeConta().setSeguinteFuncionario(new Gerente().setSeguinteFuncionario(new Diretor()));
 
+        handlerBase.processarSolicitacao(7800);
         handlerBase.processarSolicitacao(780000);
-        handlerBase.processarSolicitacao(780000);
-        handlerBase.processarSolicitacao(780000);
+        handlerBase.processarSolicitacao(78000);
     }
 
 }

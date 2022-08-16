965d50b9e3445c25af79f3c96c3fca53c23cda2a
victorleao <leaovic@icloud.com>
2022-08-16 15:25:18 -0300

    Deitel Comparison release

--- src/Deitel/Comparison/Comparison.java
+++ src/Deitel/Comparison/Comparison.java
@@ -3,12 +3,39 @@
 
 package Deitel.Comparison;
 
+import java.util.Scanner;
+
 public class Comparison {
     // método que inicia a execução do aplicativo java
     public static void main(String[] args) {
+        // Cria um Scanner para obter a entrada a partir da linha de comando
+        Scanner input = new Scanner(System.in);
 
-               
+        int number1; // primeiro número a comparar
+        int number2; // sundo número a comparar
 
+        System.out.print("Enter first integer: "); // prompt
+        number1 = input.nextInt(); // lê o primeiro número fornecido pelo usuário
 
-    }
-}
+        System.out.print("Enter second integer: "); // prompt
+        number2 = input.nextInt(); // lê o segundo número fornecido pelo usuário
+
+        if (number1 == number2)
+            System.out.printf("%d == %d%n", number1, number2);
+
+        if (number1 != number2)
+            System.out.printf("%d != %d%n", number1, number2);
+
+        if (number1 < number2)
+            System.out.printf("%d < %d%n", number1, number2);
+
+        if (number1 > number2)
+            System.out.printf("%d > %d%n", number1, number2);
+
+        if (number1 <= number2)
+            System.out.printf("%d <= %d%n", number1, number2);
+
+        if (number1 >= number2)
+            System.out.printf("%d >= %d%n", number1, number2);
+    } // Fim do método main
+} // fim da classe Comparison

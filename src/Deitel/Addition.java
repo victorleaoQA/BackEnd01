ace5d7fd5524e3f9c5ee00cdf5cd7915c663d4c9
victorleao <leaovic@icloud.com>
2022-08-16 14:58:42 -0300

    Deitel Addition

--- src/Deitel/Addition.java
+++ src/Deitel/Addition.java
@@ -5,10 +5,27 @@
 
 package Deitel;
 
+import java.util.Scanner;
+
 public class Addition {
+    // método nain inicia a execução do aplicativo java
+    public static void main(String[] args) {
 
+        // cria um Scanner para obter entrada a partir da janela de comandos
+        Scanner input = new Scanner(System.in);
 
+        int number1; // primeiro número a somar
+        int number2; // segundo número a somar
+        int sum; // soma de number1 e number2
 
+        System.out.print("Enter first integer: "); // prompt
+        number1 = input.nextInt(); // lê o primeiro número a ser somado
 
+        System.out.print("Enter a second integer: "); // prompt
+        number2 = input.nextInt(); // lê o segundo número a ser somado
 
-}
+        sum = number1 + number2; // executa soma dos números e depois armazema em sum
+
+        System.out.printf("Sum is %d%n", sum); // exibe a soma
+    } // fim do método main
+} // fim da classe Addition
